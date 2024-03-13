9f280c16a10a67f28b2955a90354d5ea5a40b3c7
ZainabAlrashidi <140021820+ZainabAlrashidi@users.noreply.github.com>
2024-03-13 00:22:59 +0000

    configure settings , urls , and add content to templates files

--- restaurant_app/views.py
+++ restaurant_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    # this view returns the main page of the programming platform
+    return render(request, 'restaurantModule/index.html')
