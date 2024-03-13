68434ecc6184080ef1211223f69cb87c9d1b4a10
ZainabAlrashidi <140021820+ZainabAlrashidi@users.noreply.github.com>
2024-03-13 22:32:34 +0000

    ad views and menu items , and menu list template and configure urls

--- restaurant_app/urls.py
+++ restaurant_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [    
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+     path('menu_items', views.menu_items),
+    path('menu_items/<int:item_id>', views.menu_item)
 ]
--- restaurant_app/views.py
+++ restaurant_app/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 def index(request):
     # this view returns the main page of the programming platform
     return render(request, 'restaurantModule/index.html')
+
+def menu_items(request):
+
+    # View to display the menu of the restaurant
+    menu_items = [
+        {'id': 1, 'name': 'Margherita Pizza', 'description': 'Tomato sauce, mozzarella, and oregano', 'price': 10},
+        {'id': 2, 'name': 'Pepperoni Pizza', 'description': 'Pepperoni, tomato sauce, mozzarella', 'price': 12},
+        {'id': 3, 'name': 'Bruschetta', 'description': 'Grilled bread garlic, tomatoes, olive oil', 'price': 6},
+        # Add more items as needed
+    ]
+    return render(request, 'restaurantModule/menu_list.html', {'menu_items': menu_items})
+
+def menu_item(request, item_id):
+    item1 = {'id': 1, 'name': 'Margherita Pizza', 'description': 'Tomato sauce, mozzarella, and oregano', 'price': 10}
+    item2 = {'id': 2, 'name': 'Pepperoni Pizza', 'description': 'Pepperoni, tomato sauce, mozzarella', 'price': 12}
+    item3 = {'id': 3, 'name': 'Bruschetta', 'description': 'Grilled bread garlic, tomatoes, olive oil', 'price': 6}
+    
+    # Find the target menu item based on ID
+    targetItem = None
+    if item1['id'] == item_id: targetItem = item1
+    if item2['id'] == item_id: targetItem = item2
+    if item3['id'] == item_id: targetItem = item3
+
+    # Redirect to menu list if item not found
+    if targetItem is None: return redirect('menu_list')
+    
+    # Pass the target item to the template
+    context = {'item': targetItem}
+    return render(request, 'restaurantModule/menu_item.html', context)
