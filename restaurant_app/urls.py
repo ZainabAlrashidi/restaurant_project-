7fe68e00642dbd52a9600308f4d945793ebfb790
ZainabAlrashidi <140021820+ZainabAlrashidi@users.noreply.github.com>
2024-04-25 20:33:28 +0000

    CRUID forms (add -edit)

--- restaurant_app/urls.py
+++ restaurant_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [    
     path('', views.index, name='index'),
      path('menu_list', views.menu_list,name="menu_list"),
-    path('menu_item/<int:item_id>', views.menu_item, name='menu_item')
+    path('menu_item/<int:item_id>', views.menu_item, name='menu_item'),
+    path('add_menu_item', views.add_menu_item, name='add_menu_item'),
+    path('edit_menu_item/<int:item_id>', views.edit_menu_item, name='edit_menu_item')
 ]
--- restaurant_app/views.py
+++ restaurant_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render,redirect
 from .models import MenuItem
+from django.http import HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
+
+
 
 
 
@@ -33,3 +37,27 @@ def menu_item(request, item_id):
     
     context = {'item': targetItem}
     return render(request, 'restaurantModule/menu_item.html', context)
+@csrf_exempt
+def add_menu_item(request):
+    if request.method == 'POST':
+        MenuItem.objects.create(
+            name=request.POST['name'],
+            description=request.POST['description'],
+            image_url=request.POST['image_url'],
+            meal_type=request.POST['meal_type'],
+            price=request.POST['price']
+        )
+        return HttpResponseRedirect('/menu_list')
+    return render(request, 'restaurantModule/add_menu_item.html')
+@csrf_exempt
+def edit_menu_item(request, item_id):
+    item = MenuItem.objects.get(id=item_id)
+    if request.method == 'POST':
+        item.name = request.POST['name']
+        item.description = request.POST['description']
+        item.image_url = request.POST['image_url']
+        item.meal_type = request.POST['meal_type']
+        item.price = request.POST['price']
+        item.save()
+        return HttpResponseRedirect('/menu_list')
+    return render(request, 'restaurantModule/edit_menu_item.html', {'item': item})
